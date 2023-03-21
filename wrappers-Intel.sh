9d780fb3a8976ba0405f081f129e5d4cb202d21e
Tom Seving <48923982+premiumkibble@users.noreply.github.com>
2023-03-20 21:22:59 -0600

    Update wrappers-Intel.sh
    Changed DockerHub Repo

--- wrappers-Intel.sh
+++ wrappers-Intel.sh
@@ -29,5 +29,5 @@ fi
 defaults write ~/Library/Preferences/org.macosforge.xquartz.X11.plist nolisten_tcp -bool false
 xhost + 127.0.0.1>/dev/null
 echo "wrap: Starting container..."
-docker pull haocsmt/sosi:wrappers && docker run -d  --rm --hostname `whoami`-wrappers --name wrappers -w /root/myhome -p 127.0.0.1:3389:3389 -e ip=$ip -e DISPLAY=host.docker.internal:0 -v ~/DockerHome/wrappers:/root -v ~/:/root/myhome -v ~/Dockerhome/wrappers/bin:/usr/local/sbin -i -t haocsmt/sosi:wrappers /bin/bash -c 'startup && /bin/bash || /bin/bash' 
+docker pull archimboldi/sosi:wrappers && docker run -d  --rm --hostname `whoami`-wrappers --name wrappers -w /root/myhome -p 127.0.0.1:3389:3389 -e ip=$ip -e DISPLAY=host.docker.internal:0 -v ~/DockerHome/wrappers:/root -v ~/:/root/myhome -v ~/Dockerhome/wrappers/bin:/usr/local/sbin -i -t archimboldi/sosi:wrappers /bin/bash -c 'startup && /bin/bash || /bin/bash' 
 exit
